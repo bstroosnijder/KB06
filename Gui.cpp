7fbbf8d84de5983973b3f423adc87548ad327387
Bram van Elderen <bram_bram10@hotmail.com>
2013-09-16 14:07:17 +0200

    Gui class/header added

---
+++ Gui.cpp
@@ -0,0 +1,7 @@
+#include "Gui.h"
+
+
+Gui::Gui()
+{
+
+};
---
+++ Gui.h
@@ -0,0 +1,19 @@
+#ifndef __GUI__H__
+#define __GUI__H__
+
+#include <irrlicht.h>
+
+	/**
+	 * @brief	This Class creates the graphical user interface element of the game	
+	 *			
+	 * @author	Bram van Elderen
+	 */
+	class Gui
+	{
+	public:
+		Gui();
+	private:
+
+	}
+
+#endif
