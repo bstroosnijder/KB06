bfc34ebb2a0e6398444894158d47fddb3e315123
MennoP <mennodanmaar@hotmail.com>
2013-09-25 14:11:15 +0200

    Comments voor doxygen toegevoegd

--- include/Game/Playground.h
+++ include/Game/Playground.h
@@ -7,6 +7,13 @@
 
 namespace Game
 {
+	/**
+	* @brief	Playground is an area the game is played on
+	*
+	*			The playground consist of Tower(s)  Monster(s)  and a Path
+	* @author	Michel van Os
+	* @author	Menno Postma
+	*/
 	class Playground
 	{
 	public:
