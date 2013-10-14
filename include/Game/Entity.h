c282848ca97dd54b6e6edb6cee595410d9818143
albertha147 <alberthaaksema@gmail.com>
2013-10-14 15:20:21 +0200

    Comments added
    not 100% sure about Entity because a couple functions are not used?
    Entity::Render?

--- include/Game/Entity.h
+++ include/Game/Entity.h
@@ -14,18 +14,60 @@ namespace Game
 	class Entity
 	{
 	public:
+		/**
+		* @brief Constructor
+		*
+		*/
 		Entity();
+
+		/**
+		 * @brief Constructor override with a Scenemanager to attach itself to
+		 * @param SceneManager is required to attach the entity scenenode to the manager
+		 * @param The PlaygroundListener is used to sent events to the playground
+		 */
 		Entity(irr::scene::ISceneManager*, PlaygroundListener*);
+		/**
+		 * @brief Destructor
+		 */
 		~Entity();
 
+		/**
+		 * @brief Update the entity
+		 * @param A float delta_timer is used for movement updates
+		 */
 		virtual void Update(float) = 0;
+
+		/**
+		 * @brief Update the position of the entity
+		 */
 		virtual void updatePosition();
+
+		/**
+		 * @brief Render the entity
+		 * @param p_sceneManager
+		 */
 		void Render(irr::scene::ISceneManager* p_sceneManager);
 		
+		/**
+		 * @brief Set the MaterialFlags of the entity
+		 * @param The MaterialFlags of the p_sceneNode will be changed
+		 */
 		void SetMaterialFlags(irr::scene::ISceneNode* p_sceneNode = NULL);
 
+		/**
+		 * @brief Return the position of the entity in a vector3df
+		 */
 		irr::core::vector3df GetPosition();
+
+		/**
+		 * @brief Set the position of the entity
+		 * @param p_position is the new position of the entity
+		 */
 		void SetPosition(irr::core::vector3df& p_position);
+
+		/**
+		 * @brief Return the SceneNode of the entity
+		 */
 		irr::scene::ISceneNode* GetSceneNode();
 
 	protected:
