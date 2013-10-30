f03a92c91aaae3687f4b38c09a4fe92d5a5094ea
albertha147 <alberthaaksema@gmail.com>
2013-10-30 13:39:08 +0100

    Final comments to terrain

--- include/Game/Terrain.h
+++ include/Game/Terrain.h
@@ -10,12 +10,12 @@ namespace Game
 	 * @brief This class generates the terrain
 	 *
 	 * @author Albert Haaksema
-	 * @todo comments
+	 * 
 	 */
 	class Terrain
 	{
 		public:
-			/**
+		    /**
 			 *@brief Constructor
 			 */
 			Terrain();
@@ -25,21 +25,24 @@ namespace Game
 			 */
 			~Terrain();
 
-			/**
-			 *@brief This function generates the terrain and returns the selectors for collision detection
-			 *@param p_sceneManager is required to add the terrain to the scene and to get the videodrivers
-			 *@param p_scale is used to scale the terrain default is 1
-			 */
+		   /**
+			* @ brief This function generates the terrain and returns the selectors for collision detection.
+			* @ param p_sceneManager is required to add the terrain to the scene and to get the videodrivers.
+			* @ param p_scale is used to scale the terrain default is 1.
+			* @ return Returns a triangleselector which can be used for collisiondetection purposes with the terrain.
+			*/
 			irr::scene::ITriangleSelector* GenerateTerrain(irr::scene::ISceneManager* p_sceneManager, float p_scale);
 
-			/**
+		   /**
 			* @brief Returns the height of the terrain at the given location.
 			* @param p_position is required to find the height of the terrain at the X and Z values of p_position.
+			* @return Returns a float representing the height of the terrain at (X,Z).
 			*/
 			irr::f32 GetTerrainHeight(irr::core::vector3df p_position);
 
 		   /**
 			* @brief Returns the terrain dimensions.
+			* @return Returns a 2 dimensional float with the dimensions of the terrain.
 			*/
 			irr::core::dimension2d<irr::f32> GetTerrainDimensions();
 
